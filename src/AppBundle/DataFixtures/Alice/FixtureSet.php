b6ea05851b969b70fed2b6fc2f78d11c5a054bc4
Jorge Atempa <atempaitm@gmail.com>
2017-08-05 23:18:45 -0700

    Add FakeData

---
+++ src/AppBundle/DataFixtures/Alice/FixtureSet.php
@@ -0,0 +1,13 @@
+<?php
+
+$manager = $this->getContainer()->get('h4cc_alice_fixtures.manager');
+
+// Get a FixtureSet with __default__ options.
+$set = $manager->createFixtureSet();
+$set->addFile(__DIR__.'/Users.yml', 'yaml');
+$set->addFile(__DIR__.'/Categories.yml', 'yaml');
+$set->addFile(__DIR__.'/Products.yml', 'yaml');
+//$set->addFile(__DIR__.'/Notes.yml', 'yaml');
+//$set->addFile(__DIR__.'/NoteProducts.yml', 'yaml');
+
+return $set;
