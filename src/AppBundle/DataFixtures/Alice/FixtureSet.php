f4f57328da90a54c673cf98e2d0431abb81eafeb
Jorge Atempa <atempaitm@gmail.com>
2017-08-07 08:32:16 -0700

    Add fake data to relation between User and Tables

--- src/AppBundle/DataFixtures/Alice/FixtureSet.php
+++ src/AppBundle/DataFixtures/Alice/FixtureSet.php
@@ -4,10 +4,10 @@ $manager = $this->getContainer()->get('h4cc_alice_fixtures.manager');
 
 // Get a FixtureSet with __default__ options.
 $set = $manager->createFixtureSet();
-$set->addFile(__DIR__.'/Users.yml', 'yaml');
 $set->addFile(__DIR__.'/BarTables.yml', 'yaml');
 $set->addFile(__DIR__.'/Categories.yml', 'yaml');
 $set->addFile(__DIR__.'/Products.yml', 'yaml');
+$set->addFile(__DIR__.'/Users.yml', 'yaml');
 //$set->addFile(__DIR__.'/Notes.yml', 'yaml');
 //$set->addFile(__DIR__.'/NoteProducts.yml', 'yaml');
 
