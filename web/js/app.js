486a497cf5b4241c72b949956b401a992ce3c8c8
Jorge Atempa <atempaitm@gmail.com>
2017-08-06 17:34:41 -0700

    [WIP] Add list of products

--- web/js/app.js
+++ web/js/app.js
@@ -1,6 +1,6 @@
 let product = {
     template: `
-    <div>
+    <section>
       <div class="field">
         <label class="label">Categoría de producto</label>
         <div class="control">
@@ -25,17 +25,22 @@ let product = {
           </div>
         </div>
       </div>
-      <div v-if="selectedProduct > 0" class="field">
+      <div v-if="selectedProduct > 0">
         <div class="field">
-          <label class="label">Cantidad</label>
+          <div class="field">
+            <label class="label">Cantidad</label>
             <div class="control">
               <input v-model="amount" class="input" type="number" min="0" max="100" placeholder="Introduce la cantidad del producto">
             </div>
+          </div>   
         </div>
-        <div class="field">
-          <h3>Total $ {{ calculateTotal }}</h3>
-        </div>      
-      </div>
+        <button class="button is-info" @click.prevent="addProduct">
+          <span class="icon is-normal">
+              <i class="fa fa-plus"></i>
+          </span>
+          <span>Agregar Producto</span>
+        </button>
+      </div>      
     </div>                   
     `,
     mounted() {
@@ -46,7 +51,7 @@ let product = {
       fetchCategories: function () {
         axios.get('/api/categories')
              .then(response => {
-               this.$store.commit('updateCategories', response.data.categories);
+               this.$store.commit('updateCatalogCategories', response.data.categories);
              })
              .catch(function (error) {
                console.log(error);
@@ -55,11 +60,19 @@ let product = {
       fetchProducts: function () {
         axios.get('/api/products')
              .then(response => {
-               this.$store.commit('updateProducts', response.data.products);
+               this.$store.commit('updateCatelogProducts', response.data.products);
              })
              .catch(function (error) {
                console.log(error);
              });
+      },
+      addProduct: function () {
+          let product = this.$store.state.catalogProducts.filter((p) => p.id === this.$store.state.selectedProduct)
+                                                         .map((p) => new Object({ name: p.name, price: p.price, amount: parseInt(this.$store.state.amount) }));
+          this.$store.commit('addProductToList', product);
+          this.$store.commit('updateSelectedCategory', 0);
+          this.$store.commit('updateSelectedProduct', 0);
+          this.$store.commit('updateAmount', 0);
       }
     },
     computed: {
@@ -88,10 +101,10 @@ let product = {
         }
       },
       categories () {
-        return this.$store.state.categories;
+        return this.$store.state.catalogCategories;
       },
       products () {
-        return this.$store.state.products;
+        return this.$store.state.catalogProducts;
       },
       calculateTotal () {
         return this.$store.getters.calculateTotal;
@@ -102,14 +115,61 @@ let product = {
     }
 };
 
+let productList = {
+  template: `
+  <div v-show="productListLength > 0" class="field">
+    <label class="label">Lista de productos</label>
+    <div class="control">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cant.</th>
+            <th>Subt.</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th colspan="4">Total $</th>
+            <th>{{ totalCost }}</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr v-for="(product, index) in getProducts" :key="product.id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ product[index].name }}</td>
+            <td>{{ product[index].price }}</td>
+            <td>{{ product[index].amount }}</td>
+            <td>{{ product[index].amount * product[index].price }}</td>
+          </tr>
+        </tbody>
+      </table>    
+    </div>
+  </div>
+  `,
+  computed: {
+    getProducts() {
+      return this.$store.state.products;
+    },
+    productListLength() {
+      return this.$store.state.products.length;
+    },
+    totalCost() {
+      return this.$store.state.total;
+    }
+  }
+};
+
 let note = {
     template: `
-      <div class="field">
-        <label class="label">Número de la mesa</label>
-        <div class="control">
-          <input v-model="tableNumber" class="input" type="number" min="0" max="100" placeholder="Introduce el número de la mesa">
-        </div>
+    <div class="field">
+      <label class="label">Número de la mesa</label>
+      <div class="control">
+        <input v-model="tableNumber" class="input" type="number" min="0" max="100" placeholder="Introduce el número de la mesa">
       </div>
+    </div>
     `,
     computed: {
       tableNumber: {
@@ -129,12 +189,13 @@ const store = new Vuex.Store({
     state: {
       lastNumberNote: 0,
       tableNumber: 0,
-      categories: [],
-      products: [],
+      catalogCategories: [],
+      catalogProducts: [],
       selectedCategory: 0,
       selectedProduct: 0,
       amount: 0,
-      total: 0
+      total: 0,
+      products: []
     },
     mutations: {
       updateLastNumberNote(state) {
@@ -143,17 +204,20 @@ const store = new Vuex.Store({
       updateTableNumber(state, tableNumber) {
         state.tableNumber = tableNumber;
       },
-      updateCategories(state, categories) {
-        state.categories = categories;
+      updateCatalogCategories(state, categories) {
+        state.catalogCategories = categories;
       },
-      updateProducts(state, products) {
-        state.products = products;
+      updateCatelogProducts(state, products) {
+        state.catalogProducts = products;
       },
       updateSelectedCategory(state, selectedCategory) {
         state.selectedCategory = selectedCategory;
       },
       updateSelectedProduct(state, selectedProduct) {
         state.selectedProduct = selectedProduct;
+      },
+      addProductToList(state, product) {
+        state.products.push(product);
       },
       updateAmount(state, amount) {
         state.amount = amount;
@@ -164,11 +228,11 @@ const store = new Vuex.Store({
     },
     getters: {
       calculateTotal: state => {
-        state.total = parseFloat(state.products.filter((p) => p.id === state.selectedProduct).map((x) => x.price)) * state.amount;
+        state.total = parseFloat(state.catalogProducts.filter((p) => p.id === state.selectedProduct).map((x) => x.price)) * state.amount;
         return state.total > 0 ? state.total : 0;
       },
       getProductsByCategory: state => {
-        return state.products.filter((p) => p.category === state.selectedCategory);
+        return state.catalogProducts.filter((p) => p.category === state.selectedCategory);
       }
     }
 });
@@ -177,7 +241,7 @@ new Vue({
   delimiters: ['${', '}'],
   el: 'main',
   store,
-  components: { note, product },
+  components: { note, productList, product },
   mounted() {
     this.getLastNoteNumber();
   },
