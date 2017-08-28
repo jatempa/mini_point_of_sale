4990dcd37cf7c7df640f2011da43581fb54cfede
Jorge Atempa <atempaitm@gmail.com>
2017-08-27 21:03:45 -0700

    Change font size in final part of report

--- src/AppBundle/Controller/BuilderReportController.php
+++ src/AppBundle/Controller/BuilderReportController.php
@@ -890,6 +890,7 @@ class BuilderReportController extends Controller
                         }
                     }
                     $printer->text(str_pad("_", 42,'_'));
+                    $printer->selectPrintMode(Printer::MODE_DOUBLE_HEIGHT);
                     $printer->text(str_pad("Subtotal $", 32,' ', STR_PAD_LEFT));
                     $printer->text(str_pad(number_format($subtotal,2, '.', ','),10,' ',STR_PAD_LEFT));
                     $servicio = $subtotal * 0.05;
@@ -958,6 +959,7 @@ class BuilderReportController extends Controller
                         }
                     }
                     $printer->text(str_pad("_", 42,'_'));
+                    $printer->selectPrintMode(Printer::MODE_DOUBLE_HEIGHT);
                     $printer->text(str_pad("Subtotal $", 32,' ', STR_PAD_LEFT));
                     $printer->text(str_pad(number_format($subtotal,2, '.', ','),10,' ',STR_PAD_LEFT));
                     $servicio = $subtotal * 0.05;
